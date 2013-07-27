4a99825a185f2070c72895f9e4bb62f8500f687c
Michał Dudek <michal@michaldudek.pl>
2013-07-27 16:56:18 +0200

    Added InvalidArgumentException class for easier generating of invalid argument exception messages.

---
+++ src/MD/Foundation/Exceptions/InvalidArgumentException.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Invalid argument exception that easily generates the exception message.
+ * 
+ * @package Foundation
+ * @subpackage Exceptions
+ * @author Michał Dudek <michal@michaldudek.pl>
+ * 
+ * @copyright Copyright (c) 2013, Michał Dudek
+ * @license MIT
+ */
+namespace MD\Foundation\Exceptions;
+
+use InvalidArgumentException as Base_InvalidArgumentException;
+use MD\Foundation\Debug\Debugger;
+
+class InvalidArgumentException extends Base_InvalidArgumentException
+{
+
+    /**
+     * Constructor.
+     * 
+     * @param string $expected Expected type.
+     * @param mixed $actual Actual argument given.
+     * @param int $number [optional] Argument number.
+     */
+    public function __construct($expected, $actual, $number = 1) {
+        $trace = Debugger::getPrettyTrace(debug_backtrace());
+        $type = Debugger::getType($actual);
+
+        if (isset($trace[1])) {
+            $message = $trace[1]['function'] .' expected argument '. $number .' to be '. $expected .', '. $type .' given.';
+        } else {
+            $message = 'Expected argument '. $number .' to be '. $expected .', '. $type .' given.';
+        }
+
+        parent::__construct($message);
+    }
+
+}
